ab981f13aa5224b20bb4340aad2cd4b4601156f5
daophilac <daophilac@gmail.com>
2019-04-12 22:50:51 +0700

    commit

--- Project/AppBaoThuc/app/src/main/java/com/example/appbaothuc/ChallengeDialogFragment.java
+++ Project/AppBaoThuc/app/src/main/java/com/example/appbaothuc/ChallengeDialogFragment.java
@@ -1,6 +1,9 @@
 package com.example.appbaothuc;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -10,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,13 +24,19 @@ import android.widget.Toast;
 import com.example.appbaothuc.services.AlarmService;
 import com.example.appbaothuc.services.ServiceCreator;
 
+import static android.content.Context.AUDIO_SERVICE;
+
 
 // TODO: WARNING: This class has some high logical handles
-enum ChallengeType{
+enum ChallengeType {
     Math, Shake, Qrcode
 }
+
 public class ChallengeDialogFragment extends DialogFragment implements MathChallengeFragment.OnFinishChallengeListener{
     private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+    private int currentSystemVolume;
+    private int fixedVolume;
     private String musicFilePath = "/sdcard/download/boss battle a.flac"; //TODO: Hard-coded
     private ChallengeType challengeType = ChallengeType.Math; // TODO: Hard-coded
     private boolean graduallyIncreaseVolume = true; //TODO: Hard-coded
@@ -45,8 +55,10 @@ public class ChallengeDialogFragment extends DialogFragment implements MathChall
     private boolean snoozeAgain = false;
     private Thread threadSnooze;
 
-    public ChallengeDialogFragment(){}
-    public static ChallengeDialogFragment newInstance(String title){
+    public ChallengeDialogFragment() {
+    }
+
+    public static ChallengeDialogFragment newInstance(String title) {
         ChallengeDialogFragment challengeDialogFragment = new ChallengeDialogFragment();
         challengeDialogFragment.setCancelable(false);
         return challengeDialogFragment;
@@ -56,8 +68,79 @@ public class ChallengeDialogFragment extends DialogFragment implements MathChall
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_challenge_dialog, container, false);
+
+        audioManager = (AudioManager) getContext().getSystemService(AUDIO_SERVICE);
+        buttonOk = view.findViewById(R.id.button_ok);
+        buttonCancel = view.findViewById(R.id.button_cancel);
+        buttonSnooze = view.findViewById(R.id.button_snooze);
+
+        getDialog().setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE){
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        buttonSnooze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Snooze for " + String.valueOf(snoozeTime) + " seconds.", Toast.LENGTH_LONG).show();
+                SnoozeManager snoozeManager = new SnoozeManager(mediaPlayer, snoozeTime);
+                if (threadSnooze == null || !threadSnooze.isAlive()) {
+                    threadSnooze = new Thread(snoozeManager);
+                    threadSnooze.start();
+                }
+                else {
+                    snoozeAgain = true;
+                }
+            }
+        });
+
+        if (maxVolume) {
+            currentSystemVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+            fixedVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        }
+        else {
+
+        }
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.boss_battle_a); //TODO
+        mediaPlayer.setLooping(false);
+        if (!graduallyIncreaseVolume) {
+            mediaPlayer.start();
+        }
+        else {
+            mediaPlayer.setVolume(0, 0);
+            mediaPlayer.start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (float i = 1; i <= 1000; i++) {
+                        try {
+                            if (isDismissed) {
+                                return;
+                            }
+                            if (isSnoozing) {
+                                return;
+                            }
+                            mediaPlayer.setVolume(i / 1000, i / 1000);
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (IllegalStateException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
+
         fragmentManager = getChildFragmentManager();
-        switch(challengeType){
+        switch (challengeType) {
             case Math:
                 mathChallengeFragment = new MathChallengeFragment();
                 fragmentManager.beginTransaction().replace(R.id.challenge_fragment_container, mathChallengeFragment).commit();
@@ -68,94 +151,36 @@ public class ChallengeDialogFragment extends DialogFragment implements MathChall
         return view;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        buttonOk = view.findViewById(R.id.button_ok);
-        buttonCancel = view.findViewById(R.id.button_cancel);
-        buttonSnooze = view.findViewById(R.id.button_snooze);
-
-        buttonSnooze.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Snooze for " + String.valueOf(snoozeTime) + " seconds.", Toast.LENGTH_LONG).show();
-                SnoozeManager snoozeManager = new SnoozeManager(mediaPlayer, snoozeTime);
-                if(threadSnooze == null || !threadSnooze.isAlive()){
-                    threadSnooze = new Thread(snoozeManager);
-                    threadSnooze.start();
-                }
-                else{
-                    snoozeAgain = true;
-                }
-            }
-        });
-
-
-
-
-        if(maxVolume){
-            AudioManager audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
-            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
-        }
-        mediaPlayer = MediaPlayer.create(getContext(), R.raw.boss_battle_a); //TODO
-        mediaPlayer.setLooping(false);
-        if(!graduallyIncreaseVolume){
-            mediaPlayer.start();
-        }
-        else{
-            mediaPlayer.setVolume(0,0);
-            mediaPlayer.start();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for(float i = 1; i <= 1000; i++){
-                        try {
-                            if(isDismissed){
-                                return;
-                            }
-                            if(isSnoozing){
-                                return;
-                            }
-                            mediaPlayer.setVolume(i / 1000,i / 1000);
-                            Thread.sleep(10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } catch(IllegalStateException e){
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }).start();
-        }
-    }
-    private class SnoozeManager implements Runnable{
+    private class SnoozeManager implements Runnable {
         private MediaPlayer mediaPlayer;
         private int snoozeTime;
-        public SnoozeManager(MediaPlayer mediaPlayer, int snoozeTime){
+
+        public SnoozeManager(MediaPlayer mediaPlayer, int snoozeTime) {
             this.mediaPlayer = mediaPlayer;
             this.snoozeTime = snoozeTime;
         }
-        public void run(){
+
+        public void run() {
             isSnoozing = true;
-            mediaPlayer.setVolume(0,0);
+            mediaPlayer.setVolume(0, 0);
             try {
                 Thread.sleep(this.snoozeTime * 1000);
                 isSnoozing = false;
-                for(float i = 1; i <= 1000; i++){
-                    if(isDismissed){
+                for (float i = 1; i <= 1000; i++) {
+                    if (isDismissed) {
                         return;
                     }
-                    if(snoozeAgain){
+                    if (snoozeAgain) {
                         snoozeAgain = false;
                         run();
                         return;
                     }
-                    mediaPlayer.setVolume(i / 1000,i / 1000);
+                    mediaPlayer.setVolume(i / 1000, i / 1000);
                     Thread.sleep(10);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } catch(IllegalStateException e){
+            } catch (IllegalStateException e) {
                 e.printStackTrace();
             }
         }
@@ -172,7 +197,8 @@ public class ChallengeDialogFragment extends DialogFragment implements MathChall
                 mediaPlayer.release();
                 getDialog().dismiss();
                 getActivity().finish();
-                ServiceCreator.enqueueWork(getContext(), new Intent());
+                audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentSystemVolume, 0);
             }
         });
         buttonCancel.setOnClickListener(new View.OnClickListener() {
@@ -188,4 +214,11 @@ public class ChallengeDialogFragment extends DialogFragment implements MathChall
             }
         });
     }
+
+//    @Override
+//    public void onKeyDown(int keyCode, KeyEvent keyEvent) {
+//        int s = 2;
+//        int b = s;
+//        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, fixedVolume, 0);
+//    }
 }
--- Project/AppBaoThuc/app/src/main/java/com/example/appbaothuc/MainActivity.java
+++ Project/AppBaoThuc/app/src/main/java/com/example/appbaothuc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appbaothuc;
 
+import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -7,7 +8,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
@@ -17,14 +17,13 @@ import android.os.IBinder;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.appbaothuc.services.AlarmService;
 import com.example.appbaothuc.services.NotificationService;
-import com.example.appbaothuc.services.ServiceCreator;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -50,68 +49,72 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: debug purpose
-        Calendar time1 = Calendar.getInstance();
-        Calendar time2 = Calendar.getInstance();
-        Calendar time3 = Calendar.getInstance();
-        Calendar time4 = Calendar.getInstance();
-        Calendar time5 = Calendar.getInstance();
-        Calendar time6 = Calendar.getInstance();
-        Calendar time7 = Calendar.getInstance();
-        Calendar time8 = Calendar.getInstance();
-        Calendar time9 = Calendar.getInstance();
-        Calendar time0 = Calendar.getInstance();
-        time1.add(Calendar.MINUTE, 5);
-        time2.add(Calendar.MINUTE, 3);
-        time3.add(Calendar.MINUTE, 1);
-        time4.add(Calendar.MINUTE, 4);
-        time5.add(Calendar.MINUTE, 2);
-        time6.add(Calendar.MINUTE, 6);
-        time7.add(Calendar.MINUTE, 7);
-        time8.add(Calendar.MINUTE, 8);
-        time9.add(Calendar.MINUTE, 9);
-        time0.add(Calendar.MINUTE, 10);
-        int hour1 = time1.get(Calendar.HOUR_OF_DAY);
-        int hour2 = time2.get(Calendar.HOUR_OF_DAY);
-        int hour3 = time3.get(Calendar.HOUR_OF_DAY);
-        int hour4 = time4.get(Calendar.HOUR_OF_DAY);
-        int hour5 = time5.get(Calendar.HOUR_OF_DAY);
-        int hour6 = time6.get(Calendar.HOUR_OF_DAY);
-        int hour7 = time7.get(Calendar.HOUR_OF_DAY);
-        int hour8 = time8.get(Calendar.HOUR_OF_DAY);
-        int hour9 = time9.get(Calendar.HOUR_OF_DAY);
-        int hour0 = time0.get(Calendar.HOUR_OF_DAY);
-        int minute1 = time1.get(Calendar.MINUTE);
-        int minute2 = time2.get(Calendar.MINUTE);
-        int minute3 = time3.get(Calendar.MINUTE);
-        int minute4 = time4.get(Calendar.MINUTE);
-        int minute5 = time5.get(Calendar.MINUTE);
-        int minute6 = time6.get(Calendar.MINUTE);
-        int minute7 = time7.get(Calendar.MINUTE);
-        int minute8 = time8.get(Calendar.MINUTE);
-        int minute9 = time9.get(Calendar.MINUTE);
-        int minute0 = time0.get(Calendar.MINUTE);
-        List<Integer> listRepeatDay1 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay2 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay3 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay4 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay5 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay6 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay7 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay8 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay9 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        List<Integer> listRepeatDay0 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        databaseHandler.insertAlarm(true, hour1, minute1, listRepeatDay1);
-        databaseHandler.insertAlarm(true, hour2, minute2, listRepeatDay2);
-        databaseHandler.insertAlarm(true, hour3, minute3, listRepeatDay3);
-        databaseHandler.insertAlarm(true, hour4, minute4, listRepeatDay4);
-        databaseHandler.insertAlarm(true, hour5, minute5, listRepeatDay5);
-        databaseHandler.insertAlarm(true, hour6, minute6, listRepeatDay6);
-        databaseHandler.insertAlarm(true, hour7, minute7, listRepeatDay7);
-        databaseHandler.insertAlarm(true, hour8, minute8, listRepeatDay8);
-        databaseHandler.insertAlarm(true, hour9, minute9, listRepeatDay9);
-        databaseHandler.insertAlarm(true, hour0, minute0, listRepeatDay0);
-        Intent intent = new Intent(this, NotificationService.class);
-        startService(intent);
+//        Calendar time1 = Calendar.getInstance();
+//        Calendar time2 = Calendar.getInstance();
+//        Calendar time3 = Calendar.getInstance();
+//        Calendar time4 = Calendar.getInstance();
+//        Calendar time5 = Calendar.getInstance();
+//        Calendar time6 = Calendar.getInstance();
+//        Calendar time7 = Calendar.getInstance();
+//        Calendar time8 = Calendar.getInstance();
+//        Calendar time9 = Calendar.getInstance();
+//        Calendar time0 = Calendar.getInstance();
+//        time1.add(Calendar.MINUTE, 5);
+//        time2.add(Calendar.MINUTE, 3);
+//        time3.add(Calendar.MINUTE, 1);
+//        time4.add(Calendar.MINUTE, 4);
+//        time5.add(Calendar.MINUTE, 2);
+//        time6.add(Calendar.MINUTE, 6);
+//        time7.add(Calendar.MINUTE, 7);
+//        time8.add(Calendar.MINUTE, 8);
+//        time9.add(Calendar.MINUTE, 9);
+//        time0.add(Calendar.MINUTE, 10);
+//        int hour1 = time1.get(Calendar.HOUR_OF_DAY);
+//        int hour2 = time2.get(Calendar.HOUR_OF_DAY);
+//        int hour3 = time3.get(Calendar.HOUR_OF_DAY);
+//        int hour4 = time4.get(Calendar.HOUR_OF_DAY);
+//        int hour5 = time5.get(Calendar.HOUR_OF_DAY);
+//        int hour6 = time6.get(Calendar.HOUR_OF_DAY);
+//        int hour7 = time7.get(Calendar.HOUR_OF_DAY);
+//        int hour8 = time8.get(Calendar.HOUR_OF_DAY);
+//        int hour9 = time9.get(Calendar.HOUR_OF_DAY);
+//        int hour0 = time0.get(Calendar.HOUR_OF_DAY);
+//        int minute1 = time1.get(Calendar.MINUTE);
+//        int minute2 = time2.get(Calendar.MINUTE);
+//        int minute3 = time3.get(Calendar.MINUTE);
+//        int minute4 = time4.get(Calendar.MINUTE);
+//        int minute5 = time5.get(Calendar.MINUTE);
+//        int minute6 = time6.get(Calendar.MINUTE);
+//        int minute7 = time7.get(Calendar.MINUTE);
+//        int minute8 = time8.get(Calendar.MINUTE);
+//        int minute9 = time9.get(Calendar.MINUTE);
+//        int minute0 = time0.get(Calendar.MINUTE);
+//        List<Integer> listRepeatDay1 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay2 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay3 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay4 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay5 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay6 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay7 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay8 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay9 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        List<Integer> listRepeatDay0 = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
+//        databaseHandler.insertAlarm(true, hour1, minute1, listRepeatDay1);
+//        databaseHandler.insertAlarm(true, hour2, minute2, listRepeatDay2);
+//        databaseHandler.insertAlarm(true, hour3, minute3, listRepeatDay3);
+//        databaseHandler.insertAlarm(true, hour4, minute4, listRepeatDay4);
+//        databaseHandler.insertAlarm(true, hour5, minute5, listRepeatDay5);
+//        databaseHandler.insertAlarm(true, hour6, minute6, listRepeatDay6);
+//        databaseHandler.insertAlarm(true, hour7, minute7, listRepeatDay7);
+//        databaseHandler.insertAlarm(true, hour8, minute8, listRepeatDay8);
+//        databaseHandler.insertAlarm(true, hour9, minute9, listRepeatDay9);
+//        databaseHandler.insertAlarm(true, hour0, minute0, listRepeatDay0);
+//        Intent intent = new Intent(this, NotificationService.class);
+//        startService(intent);
+
+
+//        Intent intent = new Intent(this, ChallengeActivity.class);
+//        startActivity(intent);
         // TODO
 
 
@@ -262,5 +265,24 @@ public class MainActivity extends AppCompatActivity {
             };
             service.startForeground(NOTIFICATION_ID, notification);
         }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        int a = 1;
+        int b = a;
+        return true;
+//        return super.onKeyDown(keyCode, event);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+//                .getSystemService(Context.ACTIVITY_SERVICE);
+//
+//        activityManager.moveTaskToFront(getTaskId(), 0);
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
